b399fb4fb2a9abcdc2e8307c1a4a71658937b02f
zhaocheng <zhaocheng@westwin.com>
2020-08-13 09:02:03 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo docker pull 542153354/antdesign.docs.wasm:v1.0 
+containerId="`sudo docker ps | grep "8900->80" | awk  '{print $1}'`"
+echo "containerId:$containerId"
+if [ -n "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+sudo docker run -d -p 8900:80 --restart=always 542153354/antdesign.docs.wasm:v1.0 /bin/sh 
+
+exit
